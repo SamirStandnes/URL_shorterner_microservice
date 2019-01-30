d12c5abc49cd164214e97d50f7ce46e27c73aceb
Samir Standnes <samir.standnes@lanekassen.no>
2019-01-30 09:54:22 +0100

    add index.html

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -12,7 +13,9 @@ var port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 
 
